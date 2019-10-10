7783026adb602e35ee4fefda264f259a2d94dd8d
northerlyStar <wlgcjrcfl@gmail.com>
2019-10-10 09:50:17 +0800

    code排版调整

--- js/header.js
+++
@@ -1,9 +0,0 @@
-/**
- * Created by 60331 on 2017/3/29.
- */
-// header的子目录隐藏与显示
-$(document).ready(function () {
-    $(".menu").hover(function () {
-        $(this).children("ul").slideToggle();
-    });
-});
